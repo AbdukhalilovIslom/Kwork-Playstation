32ae6e35d5f165a9f88040591c4b69314b53eac4
AbdukhalilovIslom <abdukhalilovislom2021@gmail.com>
2025-07-18 01:48:31 +0500

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import './App.css';
-// import Replenishment from './pages/Replenishment';
-// import SubscriptionPage from './pages/SubscriptionPage';
-import Games from './pages/Games';
-function App() {
-	return (
-		<>
-			{/* <SubscriptionPage />
-			<Replenishment /> */}
-			<Games />
-		</>
-	);
-}
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Games from "./pages/Games";
+import Replenishment from "./pages/Replenishment";
+import SubscriptionPage from "./pages/SubscriptionPage";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/games" element={<Games />} />
+        <Route path="/replenish" element={<Replenishment />} />
+        <Route path="/subscription" element={<SubscriptionPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
